ef4a2136fab17b0402f42ce47580f697fbdc6a64
Saj Panchal <Spanchal8146@conestogac.on.ca>
2021-03-12 01:17:12 +0000

    Merged PR 72: input validation added to profile page form.
    Joi-browser installed.
    handleChange and handleSubmit methods added for profile form.
    input validation code added.
    dynamic alert text added for input validation errors.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -11,6 +11,7 @@ import {
   Menu,
   Card,
 } from "@material-ui/core";
+import Joi from "joi-browser";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = () => ({
@@ -47,18 +48,152 @@ const styles = () => ({
 });
 class Profile extends React.Component {
   state = {
-    inputs: {
+    data: {
+      firstname: "",
+      lastname: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+    passwordData: {
+      newPassword: "",
+      reEnterPassword: "",
+    },
+    errors: {},
+    input: {
       firstname: true,
       lastname: true,
       username: true,
       email: true,
-      newpassword: true,
+      password: true,
     },
   };
+  schema = {
+    firstname: Joi.string().required().min(2).max(30),
+    lastname: Joi.string().required().min(2).max(30),
+    email: Joi.string().email().required(),
+    username: Joi.string().required().min(2).max(30),
+    password: Joi.string().required().min(2).max(30),
+  };
+  passwordSchema = {
+    newPassword: Joi.string().required().min(6).max(30),
+    reEnterPassword: Joi.string().required().min(6).max(30),
+  };
+  componentDidMount() {
+    const data = { ...this.state.data };
+    data.firstname = "John";
+    data.lastname = "Smith";
+    data.username = "jsmith";
+    data.email = "jsmith@gmail.com";
+    data.password = "123456";
+    this.setState({ data: data });
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    let { data, input, errors, passwordData } = { ...this.state };
+    const { error } = Joi.validate(data, this.schema);
+    const passwordError =
+      input.password === false
+        ? Joi.validate(passwordData, this.passwordSchema)
+        : null;
+    // if new password is entered
+    if (error) {
+      for (let item of error.details) {
+        errors[item.path[0]] = item.message;
+      }
+      this.setState({ errors: errors });
+      console.log("error:", errors);
+    } else if (passwordError && passwordError.error) {
+      for (let item of passwordError.error.details) {
+        errors[item.path[0]] = item.message;
+      }
+      console.log(passwordError.error);
+      this.setState({ errors: errors });
+      console.log(errors);
+    } else {
+      errors = {};
+      if (input.password === false) {
+      }
+      console.log(data);
+      data.password =
+        input.password === false ? passwordData.reEnterPassword : data.password;
+      passwordData["newPassword"] = "";
+      passwordData["reEnterPassword"] = "";
+      input = {
+        firstname: true,
+        lastname: true,
+        username: true,
+        email: true,
+        password: true,
+      };
+      this.setState({
+        data: data,
+        input: input,
+        errors: errors,
+        passwordData: passwordData,
+      });
+      console.log("submission sucessful");
+      console.log("data:", this.state.data);
+      console.log("password:", this.state.passwordData);
+      console.log("errors:", this.state.errors);
+    }
+  };
+  handleChange = (event) => {
+    const input = { [event.currentTarget.name]: [event.currentTarget.value] };
+    console.log(event.currentTarget.name);
+    const { data, passwordData, errors, inputs } = { ...this.state };
+
+    const schemaType =
+      event.currentTarget.name === "newPassword" ||
+      event.currentTarget.name === "reEnterPassword"
+        ? "password"
+        : "regular";
+
+    //if the target is password fields
+    const schema =
+      schemaType === "regular"
+        ? {
+            [event.currentTarget.name]: this.schema[event.currentTarget.name],
+          }
+        : {
+            [event.currentTarget.name]: this.passwordSchema[
+              event.currentTarget.name
+            ],
+          };
+
+    input[event.currentTarget.name] = event.currentTarget.value;
+    const { error } = Joi.validate(input, schema);
+
+    if (schemaType === "password") {
+      passwordData[event.currentTarget.name] = event.currentTarget.value;
+      console.log("password changed:", passwordData);
+      //this.setState({ passwordData: passwordData, errors: errors });
+      this.setState({ passwordData: passwordData });
+    } else {
+      data[event.currentTarget.name] = event.currentTarget.value;
+      // this.setState({ data: data, errors: errors });
+      this.setState({ data: data });
+    }
+    if (error) {
+      errors[event.currentTarget.name] = error.details[0].message;
+      console.log("handle change:", error.details[0]);
+    } else {
+      if (passwordData["newPassword"] !== passwordData["reEnterPassword"]) {
+        errors["newPassword"] = "password not matching";
+        errors["reEnterPassword"] = "password not matching";
+      } else {
+        errors["newPassword"] = null;
+        errors["reEnterPassword"] = null;
+        errors[event.currentTarget.name] = null;
+        this.setState({ passwordData: passwordData, errors: errors });
+      }
+    }
+  };
   handleInputs = (name) => {
-    const inputs = { ...this.state.inputs };
-    inputs[name] = false;
-    this.setState({ inputs: inputs });
+    const input = { ...this.state.input };
+    input[name] = false;
+    this.setState({ input: input });
   };
   render() {
     const { classes } = this.props;
@@ -67,7 +202,11 @@ class Profile extends React.Component {
         <Typography className={classes.cardTitle} variant="h4">
           Your Profile
         </Typography>
-        <Box className={classes.form} component="form">
+        <Box
+          className={classes.form}
+          component="form"
+          onSubmit={this.handleSubmit}
+        >
           <Button
             className={classes.editButton}
             onClick={() => this.handleInputs("firstname")}
@@ -79,10 +218,20 @@ class Profile extends React.Component {
 
           <InputLabel className={classes.subtitles}>First Name:</InputLabel>
           <Input
-            name="firstname"
+            color="primary"
+            inputComponent="input"
+            disabled={this.state.input.firstname}
             placeholder="Enter First Name"
-            disabled={this.state.inputs.firstname}
+            type="firstname"
+            name="firstname"
+            value={this.state.data.firstname}
+            onChange={this.handleChange}
           ></Input>
+          {this.state.errors.firstname && (
+            <Typography variant="subtitle2" color="error">
+              {this.state.errors.firstname}
+            </Typography>
+          )}
           <br></br>
           <Button
             className={classes.editButton}
@@ -95,56 +244,104 @@ class Profile extends React.Component {
 
           <InputLabel className={classes.subtitles}>Last Name:</InputLabel>
           <Input
-            name="lastname"
+            color="primary"
+            inputComponent="input"
+            disabled={this.state.input.lastname}
             placeholder="Enter Last Name"
-            disabled={this.state.inputs.lastname}
+            type="lastname"
+            name="lastname"
+            value={this.state.data.lastname}
+            onChange={this.handleChange}
           ></Input>
+          {this.state.errors.lastname && (
+            <Typography variant="subtitle2" color="error">
+              {this.state.errors.lastname}
+            </Typography>
+          )}
           <br></br>
           <br></br>
           <InputLabel className={classes.subtitles}>Username:</InputLabel>
           <Input
-            name="username"
+            color="primary"
             placeholder="Enter Username"
-            disabled={this.state.inputs.username}
+            disabled="true"
+            type="text"
+            name="username"
+            value={this.state.data.username}
+            onChange={this.handleChange}
           ></Input>
+          {this.state.errors.username && (
+            <Typography variant="subtitle2" color="error">
+              {this.state.errors.username}
+            </Typography>
+          )}
           <br></br>
           <br></br>
           <InputLabel className={classes.subtitles}>Email:</InputLabel>
           <Input
+            color="primary"
+            inputComponent="input"
+            disabled="true"
+            placeholder="Enter email"
+            type="email"
             name="email"
-            placeholder="Enter Email"
-            disabled={this.state.inputs.email}
+            value={this.state.data.email}
+            onChange={this.handleChange}
           ></Input>
+          {this.state.errors.email && (
+            <Typography variant="subtitle2" color="error">
+              {this.state.errors.email}
+            </Typography>
+          )}
           <br></br>
           <Button
             className={classes.editButton}
             size="small"
-            onClick={() => this.handleInputs("newpassword")}
+            onClick={() => this.handleInputs("password")}
           >
             Edit
           </Button>
           <br></br>
           <InputLabel className={classes.subtitles}>Password:</InputLabel>
           <Input
-            name="password"
-            placeholder="Enter Password"
+            color="primary"
+            placeholder="Your password"
             disabled="true"
+            type="password"
+            name="password"
+            value={this.state.data.password}
+            onChange={this.handleChange}
           ></Input>
+
           <br></br>
-          <Box
-            visibility={this.state.inputs.newpassword ? "hidden" : "visible"}
-          >
+          <Box visibility={this.state.input.password ? "hidden" : "visible"}>
             <Input
-              name="newpassword"
-              placeholder="Enter New Password"
-              disabled={this.state.inputs.newpassword}
+              color="primary"
+              placeholder="Enter new password"
+              type="password"
+              name="newPassword"
+              value={this.state.passwordData.newPassword}
+              onChange={this.handleChange}
             ></Input>
+            {this.state.errors.newPassword && (
+              <Typography variant="subtitle2" color="error">
+                {this.state.errors.newPassword}
+              </Typography>
+            )}
             <br></br>
             <Input
-              name="re-enterpassword"
-              placeholder="Re-enter New Password"
-              disabled={this.state.inputs.newpassword}
+              color="primary"
+              placeholder="Re-enter new password"
+              type="password"
+              name="reEnterPassword"
+              value={this.state.passwordData.reEnterPassword}
+              onChange={this.handleChange}
             ></Input>
+            {this.state.errors.reEnterPassword && (
+              <Typography variant="subtitle2" color="error">
+                {this.state.errors.reEnterPassword}
+              </Typography>
+            )}
           </Box>
           <br></br>
           <br></br>
